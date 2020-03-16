66832ee6e3865a098d781cfe5fedb5b7439ae3bc
Amit Fortus <amit.fortus4@gmail.com>
2020-03-16 11:28:01 +0200

    Updates

---
+++ precache-manifest.9d6b5fcc693c38a8bcf934153f7df26d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4379be34d4e48ad5d1a914d1f04d1dc2",
+    "url": "/CalCorona/index.html"
+  },
+  {
+    "revision": "06b2da9212e8c3256ab6",
+    "url": "/CalCorona/static/css/main.9d151e50.chunk.css"
+  },
+  {
+    "revision": "4e015729b5c1b69aee87",
+    "url": "/CalCorona/static/js/2.a0368e8b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/CalCorona/static/js/2.a0368e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b2da9212e8c3256ab6",
+    "url": "/CalCorona/static/js/main.2ded479a.chunk.js"
+  },
+  {
+    "revision": "fba4f6591d98d6c5c130",
+    "url": "/CalCorona/static/js/runtime-main.54cd95a7.js"
+  }
+]);
